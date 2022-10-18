285c63c3a639074869667a7678a177c42bc78434
Tiago Santos <tiago.santos@fanduel.com>
2022-10-18 22:41:12 +0100

    first simple tests

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,6 +1,43 @@
+import axios from "axios";
+import { getApi, getVerion } from ".";
+import * as constants from './constants';
+import { testFunc } from "./constants";
 
-describe('Example test', () => {
-  it('should be true for true', () => {
-    expect(true).toBe(true);
+describe('version / constant', () => {
+  it('should return the true value', () => {
+    expect(getVerion()).toBe('1.0.0');
   });
+
+  it('should allow me to mock the value', () => {
+    Object.defineProperty(constants, 'version', {value: '2.0.0'})
+    expect(getVerion()).toBe('2.0.0');
+  })
 });
+
+describe('function', () => {
+  it('should allow me to test the real return value', () => {
+    expect(testFunc()).toBe('1')
+  })
+
+  it('should allow me to test the real return value', () => {
+    const fSpy = jest.spyOn(constants, 'testFunc')
+    fSpy.mockReturnValueOnce('mocked value')
+    expect(testFunc()).toBe('mocked value')
+  })
+})
+
+describe('axios module', () => {
+  it('should mock the axios call', async () => {
+    const axiosGetSpy = jest.spyOn(axios, 'get');
+    axiosGetSpy.mockImplementationOnce(async () => ({data: 'test'}))
+    expect(await getApi()).toBe('test')
+  })
+})
+
+describe('window', () => {
+  it('should mock window', () => {
+    const windowSpy = jest.spyOn(global, 'window', 'get')
+    windowSpy.mockImplementationOnce(() => '123' as any)
+    expect(window).toBe('123')
+  })
+})
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,6 @@
-console.log('Hello World');
+import axios from "axios";
+import { apiUrl, version } from "./constants";
+
+export const getVerion = () => version;
+
+export const getApi = async () => (await axios.get(apiUrl)).data
